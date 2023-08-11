afa709d1fbb8864453e2f42497b0208300b178a8
Lucas Jerome Lovelace <lucasjeromelovelace@Lucass-Air.attlocal.net>
2023-08-10 21:02:52 -0500

    update

--- assets/scripts/favorites.js
+++ assets/scripts/favorites.js
@@ -1,7 +1,7 @@
 const apiKey = 'FxPVrp5JjGn7rG5QR2c3H3qhNClz0Tkp'
 const listEl = document.querySelector('#events ul')
 const favoritesListEl = document.querySelector('#favorites')
-// const apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&keyword=concert`;
+
 const api2URL = `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&city=nashville&apikey=${apiKey}`
 fetch(api2URL)
   .then(response => response.json())
@@ -9,84 +9,87 @@ fetch(api2URL)
     let events = data._embedded.events;
     for (let i = 0; i < events.length; i++) {
       let liEl = document.createElement('li');
-      liEl.className = 'event-item';  // Add class to the li element
+      liEl.classList.add('event-item'); // Add class to the li element
+
       let eventName = document.createElement('h3');
-      eventName.id = `event-name-${i}`;  // Add id to the h3 element
+      eventName.id = `event-name-${i}`;
+      eventName.classList.add('event-name'); // Add class to the h3 element
       eventName.textContent = events[i].name;
+
       let dateEl = document.createElement('p');
-      dateEl.id = `event-date-${i}`;  // Add id to the p element
+      dateEl.id = `event-date-${i}`;
+      dateEl.classList.add('event-date'); // Add class to the p element
       let date = new Date(events[i].dates.start.dateTime);
       dateEl.textContent = date.toLocaleString();
+
       let venue = document.createElement('p');
-      venue.id = `event-venue-${i}`;  // Add id to the p element
+      venue.id = `event-venue-${i}`;
+      venue.classList.add('event-venue'); // Add class to the p element
       venue.textContent = events[i]._embedded.venues[0].name;
+
       let image = document.createElement('img');
-      image.id = `event-image-${i}`;  // Add id to the img element
-      image.className = 'event-image';  // Add class to the img element
+      image.id = `event-image-${i}`;
+      image.classList.add('event-image'); // Add class to the img element
       image.src = events[i].images[0].url;
+
       let btn = document.createElement('button');
-      btn.id = `buy-button-${i}`;  // Add id to the button element
-      btn.className = 'buy-button';  // Add class to the button element
+      btn.id = `buy-button-${i}`;
+      btn.classList.add('buy-button'); // Add class to the button element
       btn.textContent = 'Buy Tickets';
       btn.onclick = function() {
         window.location.assign(events[i].url);
       };
+
       let btnAddToFavorites = document.createElement('button');
-      btnAddToFavorites.id = `favorites-button-${i}`;  // Add id to the button element
-      btnAddToFavorites.className = 'favorites-button';  // Add class to the button element
+      btnAddToFavorites.id = `favorites-button-${i}`;
+      btnAddToFavorites.classList.add('favorites-button'); // Add class to the button element
+      btnAddToFavorites.textContent = 'Add to Favorites';
 
-            btnAddToFavorites.textContent = 'Add to Favorites'
-
-            const favoritesArray = JSON.parse(localStorage.getItem('favorites')) || []
-            for (let j = 0; j < favoritesArray.length; j++) {
-                if (events[i].id === favoritesArray[j].id) {
-                    isButtonClicked = true
-                    btnAddToFavorites.disabled = true
-                    btnAddToFavorites.textContent = 'Added to Favorites'
-                }
-            }
-
-            btnAddToFavorites.onclick = function () {
-                var foundFavorites = false;
-                console.log(events[i])
-                const favoritesArray = JSON.parse(localStorage.getItem('favorites')) || []
-                for (let j = 0; j < favoritesArray.length; j++) {
-                    if (events[i].id === favoritesArray[j].id) {
-                        foundFavorites = true;
-                    }
-                }
-
-                if (!foundFavorites) {
-                    favoritesArray.push(events[i])
-                    localStorage.setItem('favorites', JSON.stringify(favoritesArray))
-                    renderFavorites()
-                }
-
-                isButtonClicked = true
-                btnAddToFavorites.disabled = true
-                btnAddToFavorites.textContent = 'Added to Favorites'
-            }
-
-            liEl.append(eventName, dateEl, venue, image, btn, btnAddToFavorites)
-            listEl.append(liEl)
-            
+      const favoritesArray = JSON.parse(localStorage.getItem('favorites')) || [];
+      for (let j = 0; j < favoritesArray.length; j++) {
+        if (events[i].id === favoritesArray[j].id) {
+          isButtonClicked = true;
+          btnAddToFavorites.disabled = true;
+          btnAddToFavorites.textContent = 'Added to Favorites';
         }
-    
-        renderFavorites()
-        
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error)
-    })
+      }
 
-    function renderFavorites() {
-        favoritesListEl.innerHTML = ''
-        const favoritesArray = JSON.parse(localStorage.getItem('favorites')) || []
-        for (let i = 0; i < favoritesArray.length; i++) {
-            let liEl = document.createElement('li')
-            let ulEl = document.createElement('ul')
-            liEl.textContent = favoritesArray[i].name
-            favoritesListEl.append(ulEl)
-            ulEl.append(liEl)
+      btnAddToFavorites.onclick = function() {
+        var foundFavorites = false;
+        console.log(events[i]);
+        const favoritesArray = JSON.parse(localStorage.getItem('favorites')) || [];
+        for (let j = 0; j < favoritesArray.length; j++) {
+          if (events[i].id === favoritesArray[j].id) {
+            foundFavorites = true;
+          }
         }
+
+        if (!foundFavorites) {
+          favoritesArray.push(events[i]);
+          localStorage.setItem('favorites', JSON.stringify(favoritesArray));
+          renderFavorites();
+        }
+
+        isButtonClicked = true;
+        btnAddToFavorites.disabled = true;
+        btnAddToFavorites.textContent = 'Added to Favorites';
+      };
+
+      liEl.append(eventName, dateEl, venue, image, btn, btnAddToFavorites);
+      listEl.append(liEl);
     }
+    renderFavorites();
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+
+function renderFavorites() {
+  favoritesListEl.innerHTML = '';
+  const favoritesArray = JSON.parse(localStorage.getItem('favorites')) || [];
+  for (let i = 0; i < favoritesArray.length; i++) {
+    let liEl = document.createElement('li');
+    liEl.textContent = favoritesArray[i].name;
+    favoritesListEl.append(liEl);
+  }
+}
